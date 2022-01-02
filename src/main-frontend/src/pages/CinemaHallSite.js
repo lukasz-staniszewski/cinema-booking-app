9ac2b90ad413768f842916f5f67ef06e6fa9b2d5
Łukasz Staniszewski <lukaszstaniszewski10@gmail.com>
2022-01-02 15:30:11 +0100

    CBA-43 - cinema hall visualisation connected with backend

--- src/main-frontend/src/pages/CinemaHallSite.js
+++ src/main-frontend/src/pages/CinemaHallSite.js
@@ -1,12 +1,67 @@
-import { useState } from "react";
-
+import {useCallback, useEffect, useState} from "react";
 import styles from "./CinemaHallSite.module.css";
+import {useLocation} from "react-router-dom";
 
 const CinemaHallSite = ()=>{
-    const rows = 12;
-    const columns = 10;
-    const DUMMY_DATA = ["1 1","11 0", "11 1", "11 2"]
+    const [rows, setRows] = useState();
+    const [columns, setColumns] = useState();
+    const [reservatedSeats, setReservatedSeats] = useState([]);
     const [choosePlace, setChoosePlace] = useState([]);
+    const location = useLocation();
+    const {showtimeInfo} = location.state;
+
+    const fetchFullHall = useCallback(async () => {
+        try{
+            const response_fetch = await fetch(`cinema_halls/cinema_hall/${showtimeInfo.cinemaHallNumber}`, {headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }});
+
+            if(!response_fetch.ok){
+                throw new Error("Full cinema fetch not ok!");
+            }
+
+            else {
+                const full_hall_json = await response_fetch.json();
+                setRows(full_hall_json.nrows);
+                setColumns(full_hall_json.nseatsInRows);
+            }
+        }
+        catch (err){
+            console.log(err);
+            console.log("Cant connect do database!");
+        }
+        // eslint-disable-next-line
+    }, []);
+
+    const fetchReservations = useCallback(async () => {
+        try{
+            const response_fetch = await fetch(`show_time_seats/showtime_id/?show_time_id=${showtimeInfo.showtimeId}`, {headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }});
+
+            if(!response_fetch.ok){
+                throw new Error("Reservations fetch not ok!");
+            }
+
+            else {
+                const reservations_json = await response_fetch.json();
+                let temp_reservations = [];
+                reservations_json.map(item=>{
+                    return temp_reservations.push(`${item.rowNumber} ${item.seatInRowNumber}`);
+                });
+                setReservatedSeats(temp_reservations);
+
+            }
+        }
+        catch (err){
+            console.log(err);
+            console.log("Cant connect do database!");
+        }
+        // eslint-disable-next-line
+    }, []);
+
 
     const choosePlaceHandler = (e) => {
         if (!choosePlace.includes(e.target.id)) {
@@ -19,7 +74,7 @@ const CinemaHallSite = ()=>{
     };
 
     const chairColor = (row, column) => {
-        if(DUMMY_DATA.includes(`${row} ${column}`)) {
+        if(reservatedSeats.includes(`${row} ${column}`)) {
             return styles.occupied;
         }
         else if (choosePlace.includes(`${row} ${column}`)){
@@ -27,11 +82,16 @@ const CinemaHallSite = ()=>{
         }
         return "";
     }
-
-    console.log(choosePlace);
+    console.log("NUMBER OF HALL:")
+    console.log(showtimeInfo.cinemaHallNumber);
+    useEffect(()=> {
+        fetchFullHall();
+        fetchReservations();
+    },[fetchFullHall, fetchReservations])
 
     return (
         <div className={styles.hall}>
+            <div>
             {[...Array(rows).keys()].map((row) => (
                 <div className={styles.row} key={`${row}`}>
                     {[...Array(columns).keys()].map((column) => (
@@ -42,13 +102,18 @@ const CinemaHallSite = ()=>{
                             onClick={choosePlaceHandler}
                             key={`${row} ${column}`}
                             id={`${row} ${column}`}
-                            disabled={DUMMY_DATA.includes(`${row} ${column}`) ? true :false}
+                            disabled={reservatedSeats.includes(`${row} ${column}`) ? true :false}
                         >
-                            r: {row},c: {column}{" "}
+                            <i id={`${row} ${column}`} className="fas fa-couch"/>
                         </button>
                     ))}
                 </div>
             ))}
+            <div className={styles.screen}>
+                <p>ekran</p>
+                <div></div>
+            </div>
+            </div>
         </div>
     );
 }
